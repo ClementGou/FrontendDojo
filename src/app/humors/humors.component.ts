0ff58440b864234cd119ffcc0840db64240ff20d
ClementGou <clement.gourier@gmail.com>
2018-05-22 15:01:39 +0200

    modification html et css

--- src/app/humors/humors.component.ts
+++ src/app/humors/humors.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-humors',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HumorsComponent implements OnInit {
 
-  constructor() { }
+  isAuth = true;
+
+  constructor() {
+  }
 
   ngOnInit() {
+
   }
 
 }
